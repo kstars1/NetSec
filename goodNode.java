be35d5a747d25b63c2384e127f37a0462e0e696d
kstars1 <kstars1@students.towson.edu>
2018-12-06 15:29:56 -0500

    Update goodNode.java

--- goodNode.java
+++ goodNode.java
@@ -17,13 +17,14 @@ public class goodNode implements Runnable {
         public goodNode(String threadName, ServerNode server) {
 		thread = new Thread(this, threadName); // (1) Create a new thread.
 		System.out.println(thread.getName());
-        ServerNode server = new ServerNode()
 		thread.start(); // (2) Start the thread.
 	}
 
     public void run(){
-       Thread.sleep(10000)
-
-
+       while(true)
+       {
+        Thread.sleep(10000)
+        server.findfile();
+       }
     }
 
