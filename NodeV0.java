0e6514eaf8118d62531a8bb608af0f75814d3605
kstars1 <kstars1@students.towson.edu>
2018-12-07 18:45:23 -0500

    Create NodeV0.java

---
+++ NodeV0.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2p;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author marks
+ */
+public class NodeV0 {
+    private ArrayList<NodeV0> neighbors;
+    private ArrayList<String> files;
+    public String name;
+    
+    public NodeV0(String name){
+        neighbors = new ArrayList();
+        files = new ArrayList();
+        this.name = name;
+    }
+    public NodeV0(String name,NodeV0 n1, NodeV0 n2, NodeV0 n3){
+        neighbors = new ArrayList();
+        files = new ArrayList();
+        neighbors.add(n1);
+        neighbors.add(n2);
+        neighbors.add(n3);
+        this.name = name;
+    }
+    
+    public ArrayList<NodeV0> getNeighbors(){
+        return neighbors;
+    }
+    public ArrayList<String> getFiles(){
+        return files;
+    }
+    public void addNeighbor(NodeV0 n){
+        neighbors.add(n);
+    }
+    public void addFile(String s){
+        files.add(s);
+        //send file to central database;
+    }
+    public void removeNeighbor(NodeV0 n){
+        if (neighbors.contains(n)){
+            neighbors.remove(n);
+        } else
+            System.err.println("Error: node is not a neighbor");
+    }
+}
